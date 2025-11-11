21efe625be0767a855de3db1a0abe44255c2fc6f
Jeanelle_Eshun <jeanelle.eshun@gmail.com>
2025-11-11 13:10:12 -0500

    setting up the methods

--- matrix/Matrix.java
+++ matrix/Matrix.java
@@ -1,2 +1,9 @@
 public class Matrix {
+  private int [][] matrix;
+  public void matrix (int size){
+    matrix = new int[size][size];
+    System.out.println("The height is" + size + "and width is" + size);
+  }
+  private void swap(int x1, int y1, int x2, int y2){
+  }
 }
