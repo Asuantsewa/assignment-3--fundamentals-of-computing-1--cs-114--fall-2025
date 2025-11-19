61906c04cab98305ecefbd6e9123832587de3629
Jeanelle_Eshun <jeanelle.eshun@gmail.com>
2025-11-18 19:52:37 -0500

    couldnt fix the issue

--- matrix/RunMatrix.java
+++ matrix/RunMatrix.java
@@ -6,11 +6,5 @@ public class RunMatrix {
     System.out.print("Enter the size of the matrix: ");
     int size = scan.nextInt();
     scan.close();
-
-    Matrix matrix = new Matrix(size);
-  matrix.populateMatrix();
-    matrix.printMatrix("Printing Matrix");
-    matrix.flipMatrix();
-    matrix.printMatrix("Printing flipped Matrix: ");
   }
 }
